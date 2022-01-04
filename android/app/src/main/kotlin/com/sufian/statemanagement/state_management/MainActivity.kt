8311dae5bec9f1035b790a22494f87974b17c3e8
NRBConnectTV <NRBConnectTV@SmartIllusions-MacBook-Air.local>
2022-01-04 15:05:05 +0600

    Initial Commit

---
+++ android/app/src/main/kotlin/com/sufian/statemanagement/state_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sufian.statemanagement.state_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:state_management/providers/counter_provider.dart';
+import 'package:provider/provider.dart';
+
+class MyHomePage extends StatelessWidget{
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider Example App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+          children:  <Widget>[
+            Text('You have pushed the button this many times: ${context.watch<Counter>().count}')
+          ],
+        ),
+      ),
+      floatingActionButton: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+        FloatingActionButton(
+          onPressed: ()=>context.read<Counter>().decrement(),
+          key: const Key('decrement_floatingActionButton'),
+          tooltip: 'Decrement',
+          child: const Icon(Icons.remove),
+        ),
+        const SizedBox(width: 10,),
+        FloatingActionButton(
+          onPressed: ()=>context.read<Counter>().reset(),
+          key: const Key('reset_floatingActionButton'),
+          tooltip: 'Reset',
+          child: const Icon(Icons.exposure_zero),
+        ),
+        const SizedBox(width: 10.0,),
+        FloatingActionButton(
+          onPressed: ()=> context.read<Counter>().increment(),
+          key: const Key('increment_floatingActionButton'),
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        )
+      ],
+      ),
+    );
+  }
+}
